ed520c98efa090cbaf6db981ecc864d429764fcd
Benjamin P <contact.benjaminpaumard@gmail.com>
2021-11-18 23:19:45 +0100

    2021.11.18 - cli update
    including error message, and help option

--- src/cli/Cli.java
+++ src/cli/Cli.java
@@ -3,12 +3,27 @@
  */
 package cli;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author Benjamin PAUMARD
  *
  */
 
 class Cli {
+	public static void printHelp() {
+		Path helpFile = Paths.get("HelpContent.txt");
+		try {
+			String helpContent = Files.readString(helpFile);
+			System.out.println(helpContent);
+		} catch (IOException e) {
+			System.err.println("Something wrong happened :/");
+		}
+	}
+
 	/**
 	 * @param args
 	 */
@@ -17,6 +32,8 @@ class Cli {
 			// If no options provided, show error and help option.
 			System.err.println("You must pass at least one option, for more information, use -h or --help.");
 		}
-		else if ((args[0] == "-h") || (args[0] == "--help")) {}		
+		else if ((args[0].equals("-h")) || (args[0].equals("--help"))) {
+			printHelp();
+		}
 	}
 }
